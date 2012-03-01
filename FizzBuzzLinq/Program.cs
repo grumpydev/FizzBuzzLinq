82932a7bca35634550893172e38477be4031bde0
Steven Robbins <ste.robbins@gmail.com>
2012-03-01 20:31:22 +0000

    Initial commit

---
+++ FizzBuzzLinq/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FizzBuzzLinq
+{
+    using System;
+    using System.Linq;
+
+    using System.Globalization;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(String.Join("\n", Enumerable.Range(1, 100).Select((i) => ((i % 3 == 0) && (i % 5 == 0)) ? "FizzBuzz" : (i % 3 == 0) ? "Fizz" : (i % 5 == 0) ? "Buzz" : i.ToString(CultureInfo.InvariantCulture)).ToArray()));
+        }
+    }
+}
